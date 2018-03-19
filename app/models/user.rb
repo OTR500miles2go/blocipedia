0b9e4c19d7badcbfd31270466e2cebd75817fa51
OTR500miles2go <32044213+OTR500miles2go@users.noreply.github.com>
2018-03-19 14:30:50 -0500

    User sign in/out, Login status on navbar, Added username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
- devise :database_authenticatable, :registerable, 
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
-end
+  devise :database_authenticatable, :registerable, 
+          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+ end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180318052350) do
+ActiveRecord::Schema.define(version: 20180319050951) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +23,6 @@ ActiveRecord::Schema.define(version: 20180318052350) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
@@ -32,10 +30,14 @@ ActiveRecord::Schema.define(version: 20180318052350) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
