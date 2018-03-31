28dd56ebf5987f33bdcd08aa0f2aa4b2f369da36
OTR500miles2go <32044213+OTR500miles2go@users.noreply.github.com>
2018-03-31 16:44:53 -0500

    Private wiki control

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,18 @@ class UsersController < ApplicationController
 
   def downgrade
     current_user.standard!
+
+    @wikis = Wiki.all
+
+    if current_user.standard?
+      @wikis = Wiki.where(private: true)
+      #binding.pry
+      @wikis.each do |wiki|
+        wiki.private = false
+        wiki.save! 
+      end
+    end
+ 
   end
 
 end
